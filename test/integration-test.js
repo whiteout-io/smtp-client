8a381b70f7416762bb7be85aeb74268c86284cd1
Tankred Hase <tankred@whiteout.io>
2013-08-19 09:24:46 +0200

    try gmail over port 25

--- test/integration-test.js
+++ test/integration-test.js
@@ -10,18 +10,6 @@ if (typeof window === 'undefined') {
     expect = window.chai.expect;
 }
 
-dummyMail = {
-    from: [{
-        name: 'Whiteout Test',
-        address: 'whiteout.test@t-online.de'
-    }], // sender address
-    to: [{
-        address: 'safewithme.testuser@gmail.com'
-    }], // list of receivers
-    subject: "Hello", // Subject line
-    body: "Hello world" // plaintext body
-};
-
 describe('SmtpClient integration tests', function() {
     this.timeout(20000);
 
@@ -33,6 +21,18 @@ describe('SmtpClient integration tests', function() {
 
     describe('SmtpClient.send with T-mobile without TLS', function() {
         it('should send an email', function(done) {
+            dummyMail = {
+                from: [{
+                    name: 'Whiteout Test',
+                    address: 'whiteout.test@t-online.de'
+                }], // sender address
+                to: [{
+                    address: 'safewithme.testuser@gmail.com'
+                }], // list of receivers
+                subject: "Hello", // Subject line
+                body: "Hello world" // plaintext body
+            };
+
             loginOptions = {
                 secure: false, // use SSL
                 port: 25,
@@ -75,25 +75,37 @@ describe('SmtpClient integration tests', function() {
     //     });
     // });
 
-    // describe('SmtpClient.send with Gmail over TLS', function() {
-    //     it('should send an email', function(done) {
-    //         loginOptions = {
-    //             secure: true, // use SSL
-    //             port: 465,
-    //             host: 'smtp.gmail.com',
-    //             auth: {
-    //                 user: "safewithme.testuser@gmail.com",
-    //                 pass: "hellosafe"
-    //             }
-    //         };
+    describe('SmtpClient.send with Gmail without TLS', function() {
+        it('should send an email', function(done) {
+            dummyMail = {
+                from: [{
+                    name: 'Whiteout Test',
+                    address: 'safewithme.testuser@gmail.com'
+                }], // sender address
+                to: [{
+                    address: 'safewithme.testuser@gmail.com'
+                }], // list of receivers
+                subject: "Hello", // Subject line
+                body: "Hello world" // plaintext body
+            };
 
-    //         sc = new SmtpClient(loginOptions);
-    //         sc.send(dummyMail, function(error, response) {
-    //             expect(error).to.not.exist;
-    //             expect(response.message).to.exist;
-    //             expect(response.messageId).to.exist;
-    //             done();
-    //         });
-    //     });
-    // });
+            loginOptions = {
+                secure: false, // use SSL
+                port: 25,
+                host: 'smtp.gmail.com',
+                auth: {
+                    user: "safewithme.testuser@gmail.com",
+                    pass: "hellosafe"
+                }
+            };
+
+            sc = new SmtpClient(loginOptions);
+            sc.send(dummyMail, function(error, response) {
+                expect(error).to.not.exist;
+                expect(response.message).to.exist;
+                expect(response.messageId).to.exist;
+                done();
+            });
+        });
+    });
 });
